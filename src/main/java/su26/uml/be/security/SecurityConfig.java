a000a334a67c183bc0a9620d8ec54d0d0fd90944
Vu219 <halekaiqkahsn@gmail.com>
2026-06-14 12:02:37 +0700

    update SecurityConfig 14/6

--- src/main/java/su26/uml/be/security/SecurityConfig.java
+++ src/main/java/su26/uml/be/security/SecurityConfig.java
@@ -109,7 +109,8 @@ public class SecurityConfig {
                 "http://localhost:*",
                 "https://localhost:*",
                 "http://127.0.0.1:*",
-                "https://*.ngrok-free.app"
+                "https://*.ngrok-free.app",
+                "https://*.up.railway.app"
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
