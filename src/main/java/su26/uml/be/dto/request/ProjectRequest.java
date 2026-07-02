9a7a990b066c0f2559d9fcf439e94fc036774408
GIATHE\acer <230933186+Bach-Ho-Gia-The@users.noreply.github.com>
2026-07-02 09:16:08 +0700

    fish

--- src/main/java/su26/uml/be/dto/request/ProjectRequest.java
+++ src/main/java/su26/uml/be/dto/request/ProjectRequest.java
@@ -11,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProjectRequest {
+    @NotBlank(message = "PROJECT_NAME_REQUIRED")
     @Size(max = 255, message = "PROJECT_NAME_TOO_LONG")
     String projectName;
 
